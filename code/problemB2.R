c45b33e87fc9a3afded5007694e5e94a9b0a2979
Sindre Henriksen <sindrehenriksen93@gmail.com>
2019-01-30 10:04:56 +0100

    Add plot of number of tries versus alpha (B2)

--- code/problemB2.R
+++ code/problemB2.R
@@ -11,8 +11,8 @@ n = 10000
 
 # Simulate and calculate empirical and true means and variances
 sim = r_gamma2(n, alpha)
-n_tries = sim$n_tries
-print(n_tries)
+# n_tries = sim$n_tries
+# print(n_tries)
 samples = tibble(x = sim$x)
 empirical_mean = mean(samples$x)
 empirical_var = var(samples$x)
@@ -25,13 +25,20 @@ ggplot(data=samples) +
   stat_function(fun=function(x)dgamma(x, shape=alpha), colour="darkred")
 
 # Compare empirical means and variances with true values for different alphas
-empirical_means = empirical_vars = numeric(4)
-alphas = c(5, 100, 2000, 5000)
-for(i in 1:4) {
-  x = r_gamma2(n, alphas[i])$x
+n = 1000
+alphas = c(2, 5, 10, 20, 50, 100, 200, 500, 1000, 2000, 3000, 4000, 5000)
+empirical_means = empirical_vars = n_tries = numeric(length(alphas))
+for(i in 1:length(alphas)) {
+  sim_i = r_gamma2(n, alphas[i])
+  n_tries[i] = sim_i$n_tries
+  x = sim_i$x
   empirical_means[i] = mean(x)
   empirical_vars[i] = var(x)
 }
 true_means = true_vars = alphas
 max((empirical_means - true_means) / true_means)
 max((empirical_vars - true_vars) / true_vars)
+
+# Plot number of tries needed to generate 1000 realisations versus alpha
+ggplot(data=tibble(x=alphas, y=n_tries)) +
+  geom_line(aes(x=x, y=y))
